f6da70fbbecace8c08be967488042bbc2984c224
juliaozm <ozmiteljul@gmail.com>
2023-09-27 22:47:12 +0100

    Created a separate component - FilterSelectItem and updated a RegionFilter component

---
+++ src/components/FilterSelectItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-select
+    :value="value"
+    :items="items"
+    :placeholder="placeholder"
+    clearable
+    :clear-icon="CloseCircleIcon"
+    :loading="isLoading"
+    variant="outlined"
+    bg-color="#fff"
+    color="primary"
+    item-color="primary"
+    base-color="primary"
+    hide-no-data
+    @input="$emit('input', value)"
+  ></v-select>
+</template>
+
+<script>
+import { CloseCircleIcon } from "mdi-vue3";
+export default {
+  name: "FilterSelectItem",
+  components: { CloseCircleIcon },
+  props: {
+    value: {
+      type: String,
+    },
+    items: {
+      type: Array,
+      default: () => [],
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup() {
+    return {
+      CloseCircleIcon,
+    };
+  },
+};
+</script>
--- src/components/RegionFilter.vue
+++ src/components/RegionFilter.vue
@@ -1,44 +1,34 @@
 <template>
   <ToastError v-if="error" :message="error" />
   <div class="w-full sm:w-1/3 md:w-1/4">
-    <v-select
-      id="region"
+    <FilterSelectItem
       v-model="selectedRegion"
       :items="regions"
       placeholder="Select region"
-      variant="outlined"
-      clearable
-      :clear-icon="CloseCircleIcon"
-      bg-color="#fff"
-      color="primary"
-      item-color="primary"
-      base-color="primary"
-      :loading="isLoading"
-      hide-no-data
-      @update:modelValue="getCountriesByRegion(selectedRegion)"
-    ></v-select>
+      :isLoading="isLoading"
+    />
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { getRegions } from "../utils/countryApi";
-import { CloseCircleIcon } from "mdi-vue3";
 import ToastError from "./ToastError.vue";
+import FilterSelectItem from "./FilterSelectItem.vue";
 
 export default {
   name: "RegionFilter",
-  components: { CloseCircleIcon, ToastError },
+  components: { FilterSelectItem, ToastError },
   props: {
     getCountriesByRegion: {
       type: Function,
       required: true,
     },
   },
-  setup() {
+  setup({ getCountriesByRegion }) {
     const regions = ref([]);
     const selectedRegion = ref(null);
-    const isLoading = ref(null);
+    const isLoading = ref(false);
     const error = ref("");
 
     const getListOfRegions = async () => {
@@ -55,7 +45,7 @@ export default {
 
         setTimeout(() => {
           isLoading.value = false;
-        }, 1000);
+        }, 300);
 
         regions.value = listofregion;
       } catch (e) {
@@ -65,13 +55,17 @@ export default {
 
     getListOfRegions();
 
+    watch(selectedRegion, (newValue) => {
+      console.log("selectedRegion changed:", newValue);
+      getCountriesByRegion(newValue);
+    });
+
     return {
       regions,
       selectedRegion,
       getListOfRegions,
       isLoading,
       error,
-      CloseCircleIcon,
     };
   },
 };
