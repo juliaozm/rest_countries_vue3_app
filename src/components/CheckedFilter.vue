fb7ddf12be36c0fd9437c2b39ed54fbd52f37454
juliaozm <ozmiteljul@gmail.com>
2023-09-28 20:16:02 +0100

    Added CheckedFilter component to include/remove countries depending on the parameter

---
+++ src/components/CheckedFilter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="w-full sm:w-1/3 md:w-1/4">
+    <v-checkbox v-model="isChecked" label="Support translation" color="primary">
+      <template #label>
+        <span class="text-lg text-gray-500">Support translation</span>
+      </template>
+    </v-checkbox>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from "vue";
+
+export default {
+  name: "CheckedFilter",
+  props: {
+    getCheckedCountries: {
+      type: Function,
+      required: true,
+    },
+  },
+
+  setup({ getCheckedCountries }) {
+    const isChecked = ref(false);
+
+    watch(isChecked, (newValue) => {
+      getCheckedCountries(newValue);
+    });
+
+    return {
+      isChecked,
+    };
+  },
+};
+</script>
