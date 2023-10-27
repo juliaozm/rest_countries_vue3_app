c9257f9d061d772ef4eec03a3b017ac1f1ed1047
juliaozm <ozmiteljul@gmail.com>
2023-10-27 17:33:12 +0100

    Updated styles of the list of countries and the countries items

---
+++ src/components/UI/ButtonPrimary.vue
@@ -0,0 +1,18 @@
+<template>
+  <button
+    @click="handleClick"
+    class="tw-bg-violet-700 hover:tw-bg-violet-800 :active:tw-bg-violet-900 tw-text-white tw-font-bold tw-py-2 tw-px-4 tw-border tw-border-violet-700 tw-rounded"
+  >
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  name: "ButtonPrimary",
+  props: {
+    handleClick: {
+      type: Function,
+    },
+  },
+};
+</script>
