0bdaebb2bb38cea2665e4f83d6f08b4971b19362
juliaozm <ozmiteljul@gmail.com>
2023-10-27 17:37:58 +0100

    Minor changes in configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    maxWidth: {
+      "1/4": "25%",
+      "1/2": "50%",
+      "3/4": "75%",
+    },
     extend: {},
   },
+  prefix: "tw-",
   plugins: [],
 };
