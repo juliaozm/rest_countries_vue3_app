ddbece31acf024ed327c2208b67a211dbccfe11a
juliaozm <ozmiteljul@gmail.com>
2023-11-30 13:41:35 +0000

    Changes to router's pathes and names

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,52 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { getAuth } from "firebase/auth";
+import { appAuth } from "../../firebase.config";
+import { ref } from "vue";
+const Home = () => import("../views/Home.vue");
+const Auth = () => import("../views/AuthView.vue");
+const Country = () => import("../views/CountryView.vue");
+const MyCountries = () => import("../views/MyCountriesView.vue");
+
+const user = getAuth(appAuth);
+const authNameRoute = ref("Login");
 
 const routes = [
-  { path: "/", name: "Home", component: () => import("../views/Home.vue") },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      requiresAuth: false,
+      shown: true,
+    },
+  },
   {
     path: "/my_countries",
     name: "My Countries",
-    component: () => import("../views/MyCountriesView.vue"),
+    component: MyCountries,
+    meta: {
+      requiresAuth: false,
+      shown: true,
+    },
   },
   {
     path: "/authentication",
-    name: "Login",
-    component: () => import("../views/AuthView.vue"),
+    name: authNameRoute,
+    component: Auth,
+    meta: {
+      requiresAuth: false,
+      shown: true,
+    },
   },
   {
     path: "/:cca2",
-    component: () => import("../views/CountryView.vue"),
+    name: "Country",
+    component: Country,
     props: true,
+    meta: {
+      requiresAuth: false,
+      shown: false,
+    },
   },
 ];
 
@@ -25,4 +56,19 @@ const router = createRouter({
   linkActiveClass: "active",
 });
 
-export default router;
+router.beforeEach(async (to) => {
+  if (!user.currentUser) {
+    // The `/my_countries` route is protected and will redirect to `/authentication` if not authenticated
+    if (to.path === "/my_countries") {
+      return "/authentication";
+    }
+  }
+  return true;
+});
+
+router.afterEach(async (to) => {
+  authNameRoute.value = !user.currentUser ? "Login" : "Log out";
+  return true;
+});
+
+export { router, routes };
